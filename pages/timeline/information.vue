79256bc9351d14de45c4ba0bf93c15f91970883e
ovosol <ovosol40@gmail.com>
2023-03-26 13:53:13 +0300

    fix change video in information

--- pages/timeline/information.vue
+++ pages/timeline/information.vue
@@ -1,48 +1,46 @@
 <template>
   <div>
-    <ModuleVideo
-      :videoSrc="currentVideo"
-      :loop="true"
-    ></ModuleVideo>
+    {{chosenYear}}
+    <ModuleVideo @ended="changeTimeline()" :videoSrc="currentVideo" :loop="false"></ModuleVideo>
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
-
-const years = ['1936',
-  '1953',
-  '1961',
-  '1970',
-  '1980s',
-  '1990s',
-  '2000s',
-  '2010s',];
+import { mapGetters } from 'vuex'
 
 export default {
-  async asyncData({$axios}) {
+  async asyncData({ $axios }) {
     const chosenYear = await $axios
       .$get('/api/timeline/year/')
       .then((response) => {
-        console.log(response, 'response.data')
+        // console.log(response, 'response.data')
         return response.year
       })
 
     const currentVideo = await $axios
       .$get('/api/timeline/' + chosenYear + '/1/')
       .then((response) => {
-        console.log(response, 'response.data')
+        // console.log(response, 'response.data')
         return process.env.BASE_URL + response.current_video
       })
 
     return {
       currentVideo,
-      chosenYear
+      chosenYear,
     }
   },
   data() {
     return {
-      intro: true
+      allYears: [
+        '1936',
+        '1953',
+        '1961',
+        '1970',
+        '1980s',
+        '1990s',
+        '2000s',
+        '2010s',
+      ],
     }
   },
   watcher: {
@@ -51,7 +49,7 @@ export default {
     },
   },
   computed: {
-    ...mapGetters({videoByPath: 'btns/byPath', byPath: 'byPath'}),
+    ...mapGetters({ videoByPath: 'btns/byPath', byPath: 'byPath' }),
     timeline() {
       return this.byPath('timeline')
     },
@@ -59,30 +57,22 @@ export default {
   methods: {
     async changeTimeline() {
       let counter =
-        this.allYears.findIndex((x) => x.name === this.chosenYear) + 1
-      let newYear = this.allYears[counter].name
+        this.allYears.findIndex((x) => x === this.chosenYear) + 1
+      counter = counter===8?0:counter; 
+      let newYear = this.allYears[counter]
       if (!this.timeline.pause) {
         this.timeline.pause = false
       }
-      if (counter >= 7) {
+      // if (counter >= 7) {
         await this.$axios
-          .$post('/api/timeline/year/', {year: newYear})
+          .$post('/api/timeline/year/', { year: newYear })
           .then(function (response) {
             console.log(response)
           })
           .catch(function (error) {
             console.log(error)
           })
-      } else {
-        await this.$axios
-          .$post('/api/timeline/year/', {year: newYear})
-          .then(function (response) {
-            console.log(response)
-          })
-          .catch(function (error) {
-            console.log(error)
-          })
-      }
+      // }
     },
     // refreshData: function () {
     //   setInterval(async function () {
